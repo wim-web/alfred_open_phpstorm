8bfc68a0dce1b9e582693da7fa7febd2259be5a1
wim-web <rootuser.main+github@gmail.com>
2020-06-13 23:11:04 +0900

    :muscle: Add 指定ディレクトリ以下のファイルを読み込むとこまで実装

--- index.ts
+++ index.ts
@@ -1,5 +1,40 @@
-const { args } = Deno;
+import Setting from "./Setting.ts";
+import {Output, ScriptFilter} from "./AlfredInterface.ts";
+
+const { args, readDirSync, readTextFileSync } = Deno;
 import { parse } from "https://deno.land/std/flags/mod.ts";
+import { join } from "https://deno.land/std/path/mod.ts";
 
-console.dir(parse(args));
+const input = parse(args)._;
 
+const dirname = new URL(".", import.meta.url).pathname;
+
+const data: Setting = JSON.parse(readTextFileSync(dirname + 'setting.json')).setting;
+
+const getProject = (path: string): ScriptFilter[] => {
+
+  const items: ScriptFilter[] = [];
+
+  for (const item of readDirSync(path)) {
+    if (item.isFile) continue;
+
+    items.push({
+      type: "file",
+      title: item.name,
+      subtitle: join(data.dirs[0], item.name),
+      arg: item.name
+    })
+  }
+
+  return items;
+}
+
+const items: ScriptFilter[] = data.dirs.flatMap(path => {
+  return getProject(path);
+})
+
+const output: Output = {
+  "items": items
+}
+
+console.log(JSON.stringify(output));
